a37160b5422600b2e91fc9a001594263af14b499
wxy <1939311091@qq.com>
2023-10-23 15:34:10 +0800

    fix

--- model/moment.go
+++ model/moment.go
@@ -5,6 +5,6 @@ import "gorm.io/gorm"
 type Moment struct {
 	gorm.Model
 	UserId  int    `gorm:"not null;index"`
-	User    User   `gorm:"foreignKey:UserId;AssociationForeignKey:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	User    User   `gorm:"foreignKey:UserId;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Content string `gorm:"size:2048"`
 }
